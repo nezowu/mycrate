b6dadae8c31f24dc589155c9f8c27b8f1f3bff92
neza <zowu@list.ru>
2022-07-04 10:02:24 +0300

    fourth

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
-pub fn type_of<T>(_: &T) -> &'static str {
+pub fn type_of_ptr<T>(_: &T) -> &'static str {
+  std::any::type_name::<T>()
+}
+
+pub fn type_of<T>(_: T) -> &'static str {
   std::any::type_name::<T>()
 }
